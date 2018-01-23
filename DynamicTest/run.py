061a97ddd434d32a8796e6261979847b8b2e5088
fzm <374648064@qq.com>
2018-01-23 21:57:15 +0800

    修改Mysql测试脚本

--- DynamicTest/run.py
+++ DynamicTest/run.py
@@ -8,34 +8,34 @@ import threading
 import time
 
 #软件配置文件所在目录
-softwareConfPath = "/etc/mysql/mysql.conf.d/"
+softwareConfPath = "/etc/mysql/"
 #软件默认配置文件路径
-srcConfFilePath = "/home/fzm/Desktop/mysqld.cnf"
+srcConfFilePath = "./my.cnf.back"
 #新配置文件名称
-desConfFilePath = "mysql.cnf"
+desConfFilePath = "my.cnf"
 #重启软件命令
 restartSoftWareCommand = "sudo service mysql restart"
 #压力测试命令
-DynamicTestCommand = "testMysql"
+DynamicTestCommand = "./exe"
 #测试软件的名称
 SoftWareName = "mysqld"
 #监控结果存放的文件名
 MonitorResultFile = "resource.txt"
 #最终动态测试结果值存放的文件名
-resultFile = "result.txt"
+resultFile = "result.csv"
+#配置项组合存放的文件名
+measurement = "SAMPLE.csv"
 
+#总的CPU使用率和
+sumCPU = 0
 #总的内存使用和
 sumMem = 0
 #监控次数
 count = 0
+#平均CPU使用率
+averageCPU = 0
 #平均内存使用
 averageMem = 0
-
-#需要修改的配置项名称
-confName = [ "key_buffer_size", "sort_buffer_size", "read_buffer_size", "tmp_table_size", "join_buffer_size" ]
-#需要修改的值
-valueList = [ '16K', '32K', '64K', '128K', '256K', '512K', '1M', '8M', '16M', '32M',
-              '64M', '128M', '256M', '512M' ]
 
 '''
 buildConfFile: 用于生成配置项修改后新配置文件
@@ -57,9 +57,9 @@ def buildConfFile(srcConfFilePath, desConfFilePath, confName, value):
                 name = line.split('=', 1)
                 name[0] = name[0].strip()
                 #判断配置项是否为要修改的配置项
-                if name[0] == confName:
-                    oldValue = name[1].strip()
-                    line = line.replace(oldValue, value)
+                if name[0] in confName:
+                    oldValue = re.sub("\D", "", name[1].strip())
+                    line = line.replace(oldValue, value[confName.index(name[0])])
                 desFile.write(line)
         else:
             desFile.write(line)
@@ -77,7 +77,7 @@ def runDynamicTest():
 getMonitorResult: 获取资源使用情况
 """
 def getMonitorResult():
-    #生成监控信息文件result.txt
+    #生成监控信息文件result.csv
     os.system("top -p `pgrep " + SoftWareName + " | tr \"\\n\" \",\" | sed 's/,$//'` -b -n 1 > " + MonitorResultFile)
     monitorFile = open(MonitorResultFile, "r")
     lines = monitorFile.readlines()
@@ -88,42 +88,62 @@ def getMonitorResult():
         line = line.split()
         global sumMem
         sumMem += long(line[5])
+        global sumCPU
+        sumCPU += float(line[8])
 
 """
 saveResult: 获取资源使用情况
+CPUValue: CPU使用率
+MEMValue: 内存使用率
 """
-def saveResult(confValue, resourceValue):
+def saveResult(CPUValue, MEMValue):
     file = open(resultFile, "a")
-    file.write(confValue + ":" + str(resourceValue) + "\n")
+    file.write("," + str(CPUValue) + "," + str(MEMValue) + "\n")
     file.close()
 
 if __name__ == '__main__':
     if os.path.exists(resultFile):
         os.remove(resultFile)
-    for configName in confName:
+    #打开待测试用例集合
+    measurementFD = open(measurement, "r")
+    #读取所有数据
+    lines = measurementFD.readlines()
+    measurementFD.close()
+    #将配置项标题写入结果文件中
+    resultFD = open(resultFile, "a")
+    resultFD.write(lines[0].replace("\n", ", CPU, MEM\n"))
+    resultFD.close()
+    #得到待测试的配置项名称
+    confName = lines[0].rstrip("\n")
+    confName = confName.split(",")
+    for line in lines[1:]:
+        line = line.rstrip("\n")
         file = open(resultFile, "a")
-        file.write("\n[" + configName + "]\n")
+        file.write(line)
         file.close()
-        for value in valueList:
-            #生成新的配置文件
-            buildConfFile(srcConfFilePath, desConfFilePath, configName, value)
-            #拷贝新配置文件
-            os.system("sudo cp "+ desConfFilePath + " " + softwareConfPath)
-            #重新启动程序
-            os.system(restartSoftWareCommand)
-            #创建一个新的线程去运行动态测试程序
-            thread1 = threading.Thread(target=runDynamicTest, args=())
-            thread1.start()
-            sumMem = 0
-            count = 0
-            """
-            每隔30秒去检测程序占用资源情况
-            直到动态测试程序结束为止
-            """
-            while thread1.is_alive():
-                getMonitorResult()
-                count += 1
-                time.sleep(1)
-            #计算出资源平均使用大小
-            averageMem = sumMem/count
-            saveResult(value, averageMem)
+        ConfValue = line.split(",")
+        #生成新的配置文件
+        buildConfFile(srcConfFilePath, desConfFilePath, confName, ConfValue)
+        #拷贝新配置文件
+        os.system("sudo cp "+ desConfFilePath + " " + softwareConfPath)
+        #重新启动程序
+        os.system(restartSoftWareCommand)
+        print("restart success")
+        #创建一个新的线程去运行动态测试程序
+        thread1 = threading.Thread(target=runDynamicTest, args=())
+        thread1.start()
+        sumMem = 0
+        sumCPU = 0
+        count = 0
+        """
+        每隔30秒去检测程序占用资源情况
+        直到动态测试程序结束为止
+        """
+        while thread1.is_alive():
+            getMonitorResult()
+            count += 1
+            time.sleep(1)
+        #计算出资源平均使用大小
+        averageCPU = sumCPU/count
+        averageMem = sumMem/count
+        saveResult(averageCPU, averageMem)
